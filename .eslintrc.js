b246b5657b36a670cc73e0a7854418dd99e926f5
raonzena <seven1004z@gmail.com>
2022-07-03 16:39:43 +0900

    tsconfig.json, vite.config.ts 에 alias path 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,26 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
-module.exports = createConfig({ type: 'frontend', project: './tsconfig.json' })
+const { extends: extendConfigs, overrides } = createConfig({ type: 'frontend' })
+
+module.exports = {
+  extends: [...extendConfigs],
+  overrides: [...overrides],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['import'],
+  ignorePatterns: ['**/*.html'],
+  rules: {
+    'import/no-unresolved': 'error',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+}
